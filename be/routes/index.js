09c47814a72b5ead84e80bebe13c63b7df901ce9
Maxim Makarevich <46904947+Xemka1309@users.noreply.github.com>
2020-04-18 01:43:19 +0300

    add controllers structure

--- be/routes/index.js
+++
@@ -1,6 +0,0 @@
-const routes = require('./routes');
-
-module.exports = function(app, db) {
-  routes(app, db);
-
-};
--- be/routes/routes.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(app, db) {
-  app.post('/notes', (req, res) => {
-    console.log(req.body)
-    res.send('Hello')
-  });
-};
