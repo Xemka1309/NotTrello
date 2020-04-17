4cf9b489ab4f9d19ab74481dab1351cb2ac7fb46
Maxim Makarevich <46904947+Xemka1309@users.noreply.github.com>
2020-04-18 02:23:53 +0300

    repair front

--- fe/src/app/app.module.ts
+++ fe/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import {AppComponent} from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import {AppComponent} from './app.component';
     AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
