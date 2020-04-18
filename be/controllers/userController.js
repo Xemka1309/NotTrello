4130f0dc8866b9460d6c4eb78f0f6ea8c8adbdf3
AllYouNeedis <SuperMoon15@yandex.by>
2020-04-18 17:09:44 +0300

    add status codes to resp reg user

--- be/controllers/userController.js
+++ be/controllers/userController.js
@@ -4,9 +4,13 @@ exports.index = function (request, response) {
     response.status(200).send({message: "all ok!"})
 };
 exports.register = function (request, response) {
-
     UserService(request.body).then(result => {
         console.log(result);
+        response.status(200);
         response.send(result);
-    }).catch(err => console.log(err));
+    }).catch(err =>  {
+        console.log(err);
+        response.status(409);
+        response.send(err.message);
+    });
 };
