b2ce586cab14c658b79b48c2f400df52f8466f37
AllYouNeedis <SuperMoon15@yandex.by>
2020-05-22 14:32:33 +0300

    RELEASE CANDIDATE 0.0.0.1

---
+++ fe/src/app/modules/mark/components/mark-picker/mark-picker.component.ts
@@ -0,0 +1,26 @@
+import {ActivatedRoute} from '@angular/router';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {MarkService} from '../../../../services/mark/markService';
+import {Mark} from '../../../../models/mark';
+
+@Component({
+  selector: 'app-mark-picker',
+  templateUrl: './mark-picker.component.html',
+  styleUrls: ['./mark-picker.component.css']
+})
+export class MarkPickerComponent {
+  @Input()
+  marks: Mark[];
+  @Input()
+  selectedMarks: Mark[];
+  @Output()
+  clickMark = new EventEmitter();
+
+  constructor() {
+  }
+
+  clickOnMark(i: number) {
+    this.clickMark.emit(this.marks[i]);
+  }
+}
--- fe/src/app/modules/task/components/task-simple/task-simple.component.ts
+++ fe/src/app/modules/task/components/task-simple/task-simple.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, OnInit, Output} from '@angular/core';
 import {Task} from '../../../../models/task';
 import {MatDialog} from '@angular/material/dialog';
 import {TaskDetailsComponent} from '../task-details/task-details.component';
+import {Socket} from 'ngx-socket-io';
+import {TaskService} from '../../../../services/task/taskService';
 
 @Component({
   selector: 'app-task-simple',
@@ -14,15 +16,18 @@ export class TaskSimpleComponent implements OnInit {
   taskModel: Task;
   private penStyle = '/assets/icons/pen.svg';
 
-  constructor(public dialog: MatDialog) {
+  constructor(public dialog: MatDialog,
+              private socket: Socket,
+              private taskService: TaskService) {
   }
 
   ngOnInit(): void {
   }
 
   openDetails() {
-    const width = (window.innerWidth / 2).toString() + 'px';
-    console.log(width);
+    const calcWidth = window.innerWidth / 2;
+    const width = calcWidth.toString() + 'px';
+    const rightPos = ((window.innerWidth + calcWidth) / 2 ).toString() + 'px';
     const dialogRef = this.dialog.open(TaskDetailsComponent, {
       width,
       data: {
@@ -31,12 +36,16 @@ export class TaskSimpleComponent implements OnInit {
         title: this.taskModel.title,
         description: this.taskModel.description,
         dueTime: this.taskModel.dueTime,
-        completed: this.taskModel.completed
+        completed: this.taskModel.completed,
+        pos: rightPos
       }
     });
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
+      console.log(result);
+      this.taskModel = result;
     });
+
   }
 }
