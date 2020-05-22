7f351701ecf833bd901d8d7788c485988f1d9a9c
Maxim Makarevich <46904947+Xemka1309@users.noreply.github.com>
2020-05-22 14:24:05 +0300

    add edit task

--- fe/src/app/services/task/taskService.ts
+++ fe/src/app/services/task/taskService.ts
@@ -8,11 +8,17 @@ import {Task} from '../../models/task';
 export class TaskService {
   private baseUrl = '/api/task';
   private addTaskUrl = `${this.baseUrl}/add`;
+  private editTaskUrl = `${this.baseUrl}/edit`;
 
   constructor(private http: HttpClient) { }
 
   public addTask(task: any) {
     return this.http.post<Task>(this.addTaskUrl, task, {
+      observe: 'response'
+    });
+  }
+  public updateTask(task: any) {
+    return this.http.put<Task>(this.editTaskUrl, task, {
       observe: 'response'
     });
   }
